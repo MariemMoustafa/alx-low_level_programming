a128a2ff2c0026e0ff91e507b222839f540cdf44
MariemMoustafa <www.mariemfisal@gmail.com>
2023-08-26 12:15:20 -0700

    tttt

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print number of base16 in lowercase
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int digit = 48; /*48; demical rep of 0*/
+
+	while (digit <= 102) /*102; demical rep of f*/
+	{
+		putchar(digit);
+
+		/* after 9 we jump till 96; */
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
