e5afc68383dcc487cc436ba0b0112193bd328e2f
MariemMoustafa <www.mariemfisal@gmail.com>
2023-08-26 11:33:47 -0700

    tt

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII representation*/
+		putchar (digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
