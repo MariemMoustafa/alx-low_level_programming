3040f74e8dc66ef0156be21ab56c16fc32e9a21f
MariemMoustafa <www.mariemfisal@gmail.com>
2023-08-27 08:17:50 -0700

    tm

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * print_alphabet - print all alphabet in lowercase
+*/
+void print_alphabet(void)
+{
+	int m;
+
+	for (m = 'a'; m <= 'z'; m++)
+		_putchar(m);
+	_putchar('\n');
+}
