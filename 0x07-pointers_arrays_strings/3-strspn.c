1e21949ce67f3354d5c6e9e5121bc4956754ff78
MariemMoustafa <www.mariemfisal@gmail.com>
2023-08-28 18:02:41 -0700

    m

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,22 +1,18 @@
 #include "main.h"
-/**
- * _strspn - gets the ength of a prefix substring.
- *
- * @s : string.
- * @accept: bytes.
- * Return: unsigned int.
-*/
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int t, m;
+#include <stdio.h>
 
-	for (t = 0; s[t] != '\0'; t++)
-	{
-		for (m = 0; accept[m] != s[m]; m++)
-		{
-			if (accept[m] == '\0')
-				return (t);
-		}
-	}
-	return (t);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
 }
