f4107c98f4549ef135cb8c912acb4840f36f1c0d
MariemMoustafa <www.mariemfisal@gmail.com>
2023-08-28 18:59:40 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - add two instegers
+ *
+ * @a: int to be added to b
+ * @b: int to added to a
+ *
+ * Return: sum of a % b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
